d0533c0f5d518eada26f686d9f9a2f3b20dd340f
FedeGonzalez <198593483+federicogonzalez97@users.noreply.github.com>
2025-08-27 20:14:35 -0300

    CHG: update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,17 +1,8 @@
-// config.ts
+
 export const config = {
-  // API (backend)
   API_URL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 
-  // FRONT (este sitio; para armar redirects de OAuth)
   FRONTEND_URL: import.meta.env.VITE_FRONTEND_URL || 'http://localhost:5173',
-
-  // Opcional (solo si los usás en el front)
-  GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-  FACEBOOK_APP_ID: import.meta.env.VITE_FACEBOOK_APP_ID,
-
-  IS_DEVELOPMENT: import.meta.env.DEV,
-  IS_PRODUCTION: import.meta.env.PROD,
 };
 
 export const API_ENDPOINTS = {
@@ -23,6 +14,9 @@ export const API_ENDPOINTS = {
     PROFILE: `${config.API_URL}/auth/profile`,
     VERIFY: `${config.API_URL}/auth/verify`,
     LOGOUT: `${config.API_URL}/auth/logout`,
+  },
+  PROPERTIES: {
+    BASE: `${config.API_URL}/poperties-real-state`,
   },
 };
 
